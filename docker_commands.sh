8e41c33fcbaf0e3fa052b22d505a05e600acb78b
cesarv817 <cesarv817@gmail.com>
2026-09-11 21:52:27 -0500

    Complete Q1

---
+++ docker_commands.sh
@@ -0,0 +1,10 @@
+# Question 1 
+docker build -t linear-regression-app .
+docker tag linear-regression-app:latest linear-regression-app:v0.1.1
+docker tag linear-regression-app:latest linear-regression-app:v0.1.2
+docker push cesarv817/linear-regression-app:latest
+# Question 2
+
+# Question 3
+
+# Question 4

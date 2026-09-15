4a3c83b3e674507c8b12da5fcf7d648044821247
cesarv817 <cesarv817@gmail.com>
2026-09-15 16:04:03 -0500

    Complete Q3

--- docker_commands.sh
+++ docker_commands.sh
@@ -5,7 +5,6 @@ docker tag linear-regression-app:latest linear-regression-app:v0.1.2
 docker tag linear-regression-app:latest linear-regression-app:latest
 docker push cesarv817/linear-regression-app:latest
 # Question 2
-
 docker build -t volume-mounting .
 docker run --volume "C:\Users\cesar\OneDrive\Documents\GitHub\IEM_Fall2026_Assignments\assignment1\volume-mounting\data:/home/iem4723/data" volume-mounting
 # Question 3
